7827f15563e8c0cb456f566f2b56a423aa04beb1
Jacob Dale <jbd211@gmail.com>
2022-08-09 14:42:57 -0400

    fixed cumulative plot issue; still working on cropping

--- loop.R
+++ loop.R
@@ -85,18 +85,40 @@ dev.off()
 }
 
 #cumulative plot (sum)
-cropped_raster <- crop(dat[[1]],extent(dat[[3]]))
 
-files = list.files("/Users/jacobdale/raster_import/raw_2008_halpern/",pattern="*.tif$", full.names=TRUE)
-birds_2008 <- stack(files)
+cropped_raster <- crop(dat[[1]], extent(dat[[3]]))
+
+
+files = list.files("/Users/jacobdale/raster_import/raw_2008_halpern/",pattern="*.tif$", full.names=TRUE) 
+View(files)
+birds_2008 <- stack(dat)
 total <- calc(birds_2008, sum)
 
 
-cumul_ap <- c(dat[[1]], dat[[2]], along = "z")
+cumul_ap <- c(cropped_raster, dat[[3]], along = "z")
 View(cumul_ap)
-
 cumul_sum <- st_apply(cumul_ap, c(1,2), sum)
-plot(cumul_sum, grid)
+
+#####grid and plot, for cumulative sum
+brange_small <- st_transform(brange_small, crs = st_crs(cumul_sum))
+ext <- st_bbox(brange_small)
+c <- st_crop(cumul_sum, ext)
+x=c
+x <- st_as_stars(c)
+x2 = log(x+1)
+x3 <- x2
+x3[[1]] <- quantNorm(x3[[1]])
+image(x3)
+x_raster <- as(x3, "Raster")
+exact_extract(x_raster, brange_small_buffer, function(values, coverage_fraction)
+  sum(coverage_fraction))
+avg <- exact_extract(x_raster, grid, 'mean')
+avg
+#final plot for cumulative sum
+grid<- st_sf(grid)
+grid$avg = avg
+plot(grid)
+
 
 
 
